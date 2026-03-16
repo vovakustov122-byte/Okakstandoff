7f1a8e4fda60fb5377f6eb2cc6082ad2c0b77748
vovakustov122-byte <vovakustov122@gmail.com>
2026-03-16 18:54:52 +0300

    Update MainActivity.java

--- app/src/main/java/com/vovakustov122_byte/standoffesp/MainActivity.java
+++ app/src/main/java/com/vovakustov122_byte/standoffesp/MainActivity.java
@@ -1,1 +1,47 @@
+package com.vovakustov122_byte.standoffesp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private static final int REQUEST_MEDIA_PROJECTION = 100;
+    private MediaProjectionManager projectionManager;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        projectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        
+        Button startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(v -> startScreenCapture());
+    }
+    
+    private void startScreenCapture() {
+        Intent intent = projectionManager.createScreenCaptureIntent();
+        startActivityForResult(intent, REQUEST_MEDIA_PROJECTION);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if (requestCode == REQUEST_MEDIA_PROJECTION && resultCode == Activity.RESULT_OK) {
+            Intent serviceIntent = new Intent(this, ESPOverlayService.class);
+            serviceIntent.putExtra("resultCode", resultCode);
+            serviceIntent.putExtra("data", data);
+            startForegroundService(serviceIntent);
+            finish();
+        }
+    }
+}
